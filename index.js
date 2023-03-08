99ee19e4fca22ed700a925e118ca3916ab3abfa5
Ary Okoli <aco.umeadi@gmail.com>
2023-03-08 18:44:15 -0500

    looping complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+let message = [];
+
+function writeCards(names, events) {
+  for (let i = 0; i < names.length; i++) {
+    message.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+  }
+
+  return message;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
